6eaa63bdf1317ae64e895fed5235ff1ded24d16b
awaisazhar72 <134036866+awaisazhar72@users.noreply.github.com>
2024-01-24 23:06:11 +0500

    Mui-App

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import styled from "@emotion/styled";
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import { AppBar, Avatar, Badge, Box, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import { InputBase } from "@mui/material";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+}));
+
+const Navbar = () => {
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          LAMA DEV
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search.." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+        </Icons>
+        <UserBox>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+        </UserBox>
+        <Typography>Awais Azhar</Typography>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
